83502a9366a67c499febe460f9564c7fb2103eee
eslamsekilly <eslamsekilly@gmail.com>
2022-07-19 19:28:11 +0200

    just uploading it from my computer

---
+++ Header.js
@@ -0,0 +1,14 @@
+ export default function Header() {
+    return (
+        <header>
+            <nav className="navmenu">
+                <img src="react.png" className="logo" />
+                 <ul className="right">
+                     <li>Pricing</li>
+                     <li>Contact</li>
+                     <li>About</li>
+                </ul>
+            </nav>
+
+        </header>)
+}
---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+function Eslam(){
+    return (<div>
+        <Header />
+        <h1>Why react is the best ?</h1>
+        <ol>
+            <li>Amazing</li>
+            <li>cool</li>
+            <li>To be hired in a job</li>
+        </ol>
+        <footer><small>@ 2022 Elsekilly development. All rights reserved</small></footer>
+        </div>)
+
+
+}
+ReactDOM.render(<Eslam />, document.getElementById("root"))
